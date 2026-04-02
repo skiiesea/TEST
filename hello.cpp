5b7685ea6f01c8ced79f7652c5b23ffb1cda4adc
skiiesea <amfin@internet.ru>
2026-04-02 06:51:46 +0000

    feat: great function added

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
 
+int great(const std::string str)
+{
+    std::cout << "Greatings, " << str << std::endl;
+}
+
 int main() 
 {
     std::string name;
     std::cout << "Input your Name: " << std::endl;
     std::cin >> name;
-    std::cout << "Greatings, " << name;
+    great(name)
     return 0;
 }
