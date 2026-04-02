7fa2e41a03cb606ffaef5b6ce39d8dee481c7989
skiiesea <amfin@internet.ru>
2026-04-02 06:31:50 +0000

    fix: Fixed great

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,6 @@ int main()
     std::string name;
     std::cout << "Input your Name: " << std::endl;
     std::cin >> name;
-    std::cout << "Hello, " << name;
+    std::cout << "Hi, " << name;
     return 0;
 }
